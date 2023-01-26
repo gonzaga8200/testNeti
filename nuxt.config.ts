796f859eb8241d1b59de94591acd02cb4731496a
gonzaga82 <gonzalo.moreno@odigeo.com>
2023-01-26 19:28:57 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: ["@/assets/main.css"],
+    modules: [
+        ["@storyblok/nuxt" , {
+            accessToken: "vHOt9SjJXOPdrerdizbGbgtt"
+        }]
+    ]
+})
---
+++ storyblok/Projects.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  blok: Object,
+});
+</script>
+<template>
+  <div class="container project-page">
+    <div class="project-title">
+      <h1>{{ blok.name || "New Project" }}</h1>
+      <NuxtLink :to="{ name: 'index' }">Go Back</NuxtLink>
+    </div>
+
+    <div class="project-description">
+      <img
+        :src="blok.image?.filename || 'https://picsum.photos/300/175'"
+        :alt="blok.image?.alt"
+      />
+      <p>
+        {{
+          blok.description ||
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut accusantium perferendis molestias, dolorum, ea repellat doloremque, commodi corporis in omnis recusandae facilis laborum ipsum? Alias quos vitae facere aliquam incidunt."
+        }}
+      </p>
+    </div>
+  </div>
+</template>
